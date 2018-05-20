11684caaf77b03bfb4b39e57f5fa3769a76e29e3
Daniel Olawale <dan.olawale2@gmail.com>
2018-05-20 20:22:02 +0100

    Added usage comments and notes

--- private/config/database_config.php
+++ private/config/database_config.php
@@ -5,9 +5,9 @@
 //3. unique credentials if working with multiple developers
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "mysql_phebian3");
-define("DB_NAME", "oophp");
+define("DB_USER", "webuser"); //replace with your DB_USER
+define("DB_PASS", "mysql_pass"); //replace with your DB_PASS
+define("DB_NAME", "oophp"); //replace with your DB_NAME
 define("DSN", "mysql:host=localhost;dbname=oophp");
 
  ?>
--- public/views/pages/index.php
+++ public/views/pages/index.php
@@ -2,5 +2,6 @@
 
 <h1>{{ title }}</h1>
 <p>This is a custom MVC PHP framework</p>
+<p> Repalace the RewriteBase in the public .htaccess file with your document root </p>
 
 {% include site_footer %}
